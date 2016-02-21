e7e25219bc1d2106cfd79990907847729f272e33
evesgf <evesgf@163.com>
2016-02-21 21:47:27 +0800

    Initialization version

---
+++ Server/EasyRecord/EasyRecord/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using EasyRecord.Common;
+using EasyRecord.Respository;
+using EasyRecord.Respository.Category;
+using EasyRecord.Respository.Entitys;
+
+namespace EasyRecord.Controllers
+{
+    public class HomeController : ApiController
+    {
+        private readonly ICategoryDAL _iCategoryDAL;
+
+        public HomeController(ICategoryDAL iCategoryDAL)
+        {
+            _iCategoryDAL = iCategoryDAL;
+        }
+
+        [HttpGet]
+        public HttpResponseMessage Index()
+        {
+            //var db = new EasyRecordContext();
+            //var categorymodel = new CategoryEntity
+            //{
+            //    Name = "test"
+            //};
+            //db.Category.Add(categorymodel);
+            //db.SaveChanges();
+
+            return JsonHelper.GoToJson(_iCategoryDAL.Hello());
+        }
+    }
+}
---
+++ Server/EasyRecord/EasyRecord/Respository/Category/CategoryDAL.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyRecord.Respository.Category
+{
+    public class CategoryDAL:ICategoryDAL
+    {
+        public string Hello()
+        {
+            return "Hello World";
+        }
+    }
+}
